074c8619d7aefd4d3d39f932e1908edbe212a4e1
Maheesha Udalagama <maheesha@gmail.com>
2026-06-17 09:38:25 +0530

    add code for Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+
+      <main>
+        <Hero />
+        <Services />
+        <ResearchSolutions />
+        <Partners />
+        <Statistics />
+        <ResearchMonth />
+        <InnovationHub />
+        <Dashboard />
+        <Articles />
+        <Testimonials />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
