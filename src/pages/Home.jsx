4787fa7bfa564f53be46940455b81c44a46af2dd
Maheesha Udalagama <maheesha@gmail.com>
2026-06-17 09:43:51 +0530

    Fix Home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
-import React from "react";
+import "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Services from "../components/Services";  
+import ResearchSolutions from "../components/ResearchSolutions";
+import Partners from "../components/Partners";
+import Statistics from "../components/Statistics";
+import ResearchMonth from "../components/ResearchMonth";
+import InnovationHub from "../components/InnovationHub";
+import Dashboard from "../components/Dashboard";
+import Articles from "../components/Articles";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
